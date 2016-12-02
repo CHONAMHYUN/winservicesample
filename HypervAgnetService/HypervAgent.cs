06447d15523511399b61f04ea1d8d73c7368929f
DESKTOP-EJONHJD\gncloud <erobeta@gmail.com>
2016-12-02 17:16:44 +0900

    초기 커밋.

---
+++ HypervAgnetService/HypervAgent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Timers;
+
+namespace HypervAgnetService
+{
+    public partial class HypervAgent : ServiceBase
+    {
+        private Timer tm = null;
+
+        public HypervAgent()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            tm = new Timer();
+            this.tm.Interval = 30000;
+            this.tm.Elapsed += new System.Timers.ElapsedEventHandler(this.tm_Tick);
+            tm.Enabled = true;
+            Library.WriteError("Test window service started");
+        }
+
+        private void tm_Tick(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteError("Timer ticked and some job has been done successfully");
+        }
+
+        protected override void OnStop()
+        {
+            tm.Enabled = false;
+            Library.WriteError("Test window service stopped");
+
+        }
+    }
+}
---
+++ HypervAgnetService/Library.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HypervAgnetService
+{
+    public static class Library
+    {
+        public static void WriteError(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfiile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + ": " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static void WriteError(String Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfiile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
